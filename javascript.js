006b50b218c39d6b8e5a888d50647d75c6cb9901
fakeangel <brunosonsol@gmail.com>
2019-06-21 22:00:33 -0300

    First commit - send project

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function AumentarFont(){
+		document.getElementById("tudo").style.fontSize = "larger";
+	}
+	function DiminuirFont(){
+		document.getElementById("tudo").style.fontSize = "smaller";
+}
